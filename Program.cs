cc1f00f177e9abc7560af4e8bdcc905525f84065
GustavoPescaroli <130803844+GustavoPescaroli@users.noreply.github.com>
2023-04-14 20:05:02 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1Ex3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double diametro;
+            double resultado;
+            double raiz = 1.41;
+            double resultado2;
+            double potencia;
+
+            Console.WriteLine("valor do diametro");
+            diametro = double.Parse(Console.ReadLine());
+            resultado = diametro / raiz;
+            Console.WriteLine();
+            resultado2 = diametro / raiz;
+            Console.WriteLine();
+
+            potencia = resultado * resultado2;
+            Console.WriteLine("valor da area {0}", potencia);
+
+
+            
+            
+
+        }
+    }
+}
